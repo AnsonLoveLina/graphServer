25bafa567de846b590fbb0eab82a6e7a544cc659
huangyu0522 <384539643@qq.com>
2017-11-01 11:20:14 +0800

    假如druid的数据监控

--- src/main/java/com/hisign/config/DruidConfig.java
+++ src/main/java/com/hisign/config/DruidConfig.java
@@ -1,11 +1,17 @@
 package com.hisign.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+import jodd.util.StringUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
@@ -17,6 +23,7 @@ import java.sql.SQLException;
 @ConditionalOnMissingBean(value = DataSource.class)
 @Configuration
 @EnableTransactionManagement
+@ConfigurationProperties(prefix = "spring.datasource")
 public class DruidConfig {
     private static final Logger logger = LoggerFactory.getLogger(DruidConfig.class);
 
@@ -32,45 +39,43 @@ public class DruidConfig {
     @Value("${spring.datasource.driver-class-name}")
     private String driverClassName;
 
-//    @Value("${spring.datasource.initialSize}")
-//    private int initialSize;
-//
-//    @Value("${spring.datasource.minIdle}")
-//    private int minIdle;
-//
-//    @Value("${spring.datasource.maxActive}")
-//    private int maxActive;
-//
-//    @Value("${spring.datasource.maxWait}")
-//    private int maxWait;
-//
-//    @Value("${spring.datasource.timeBetweenEvictionRunsMillis}")
-//    private int timeBetweenEvictionRunsMillis;
-//
-//    @Value("${spring.datasource.minEvictableIdleTimeMillis}")
-//    private int minEvictableIdleTimeMillis;
-//
-//    @Value("${spring.datasource.validationQuery}")
-//    private String validationQuery;
-//
-//    @Value("${spring.datasource.testWhileIdle}")
-//    private boolean testWhileIdle;
-//
-//    @Value("${spring.datasource.testOnBorrow}")
-//    private boolean testOnBorrow;
-//
-//    @Value("${spring.datasource.testOnReturn}")
-//    private boolean testOnReturn;
-//
-//    @Value("${spring.datasource.poolPreparedStatements}")
-//    private boolean poolPreparedStatements;
-//    @Value("${spring.datasource.maxPoolPreparedStatementPerConnectionSize}")
-//    private int maxPoolPreparedStatementPerConnectionSize;
-//
-//    @Value("${spring.datasource.filters}")
-//    private String filters;
+    private int initialSize;
 
-    @Bean(name = "dataSource")
+    private int minIdle;
+
+    private int maxActive;
+
+    private int maxWait;
+
+    private int timeBetweenEvictionRunsMillis;
+
+    private int minEvictableIdleTimeMillis;
+
+    private String validationQuery;
+
+    private boolean testWhileIdle;
+
+    private boolean testOnBorrow;
+
+    private boolean testOnReturn;
+
+    private boolean poolPreparedStatements;
+
+    private int maxPoolPreparedStatementPerConnectionSize;
+
+    private String filters;
+
+    private String loginUsername;
+
+    private String loginPassword;
+
+    private String resetEnable;
+
+    private String allow;
+
+    private String deny;
+
+    @Bean(name = "dataSource",initMethod = "init",destroyMethod = "close")
     public DataSource dataSource() throws SQLException {
         logger.debug("Configruing DruidDataSoure");
         DruidDataSource druidDataSource = new DruidDataSource();
@@ -78,19 +83,131 @@ public class DruidConfig {
         druidDataSource.setDriverClassName(driverClassName);
         druidDataSource.setUsername(username);
         druidDataSource.setPassword(password);
-//        druidDataSource.setInitialSize(initialSize);
-//        druidDataSource.setMinIdle(minIdle);
-//        druidDataSource.setMaxActive(maxActive);
-//        druidDataSource.setMaxWait(maxWait);
-//        druidDataSource.setTimeBetweenEvictionRunsMillis(timeBetweenEvictionRunsMillis);
-//        druidDataSource.setMinEvictableIdleTimeMillis(minEvictableIdleTimeMillis);
-//        druidDataSource.setValidationQuery(validationQuery);
-//        druidDataSource.setTestWhileIdle(testWhileIdle);
-//        druidDataSource.setTestOnBorrow(testOnBorrow);
-//        druidDataSource.setTestOnReturn(testOnReturn);
-//        druidDataSource.setPoolPreparedStatements(poolPreparedStatements);
-//        druidDataSource.setMaxPoolPreparedStatementPerConnectionSize(maxPoolPreparedStatementPerConnectionSize);
-//        druidDataSource.setFilters(filters);
+        druidDataSource.setInitialSize(initialSize);
+        druidDataSource.setMinIdle(minIdle);
+        druidDataSource.setMaxActive(maxActive);
+        druidDataSource.setMaxWait(maxWait);
+        druidDataSource.setTimeBetweenEvictionRunsMillis(timeBetweenEvictionRunsMillis);
+        druidDataSource.setMinEvictableIdleTimeMillis(minEvictableIdleTimeMillis);
+        druidDataSource.setValidationQuery(validationQuery);
+        druidDataSource.setTestWhileIdle(testWhileIdle);
+        druidDataSource.setTestOnBorrow(testOnBorrow);
+        druidDataSource.setTestOnReturn(testOnReturn);
+        druidDataSource.setPoolPreparedStatements(poolPreparedStatements);
+        druidDataSource.setMaxPoolPreparedStatementPerConnectionSize(maxPoolPreparedStatementPerConnectionSize);
+        druidDataSource.setFilters(filters);
         return druidDataSource;
+    }
+
+    @Bean
+    public ServletRegistrationBean DruidStatViewServle2() {
+        logger.debug("Configruing DruidServlet");
+        //org.springframework.boot.context.embedded.ServletRegistrationBean提供类的进行注册.
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new StatViewServlet(), "/druid/*");
+
+        //添加初始化参数：initParams
+        //登录查看信息的账号密码.
+        if (StringUtil.isNotBlank(allow)){
+            servletRegistrationBean.addInitParameter("allow", allow);
+        }
+        if (StringUtil.isNotBlank(deny)){
+            servletRegistrationBean.addInitParameter("deny", deny);
+        }
+        servletRegistrationBean.addInitParameter("loginUsername", loginUsername);
+        servletRegistrationBean.addInitParameter("loginPassword", loginPassword);
+        //是否能够重置数据.
+        servletRegistrationBean.addInitParameter("resetEnable", resetEnable);
+        return servletRegistrationBean;
+    }
+
+
+    /**
+     * 注册一个：filterRegistrationBean
+     *
+     * @return
+     */
+
+    @Bean
+    public FilterRegistrationBean druidStatFilter2() {
+        logger.debug("Configruing DruidFilter");
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new WebStatFilter());
+        //添加过滤规则.
+        filterRegistrationBean.addUrlPatterns("/*");
+
+        //添加不需要忽略的格式信息.
+        filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
+        return filterRegistrationBean;
+    }
+
+    public void setInitialSize(int initialSize) {
+        this.initialSize = initialSize;
+    }
+
+    public void setMinIdle(int minIdle) {
+        this.minIdle = minIdle;
+    }
+
+    public void setMaxActive(int maxActive) {
+        this.maxActive = maxActive;
+    }
+
+    public void setMaxWait(int maxWait) {
+        this.maxWait = maxWait;
+    }
+
+    public void setTimeBetweenEvictionRunsMillis(int timeBetweenEvictionRunsMillis) {
+        this.timeBetweenEvictionRunsMillis = timeBetweenEvictionRunsMillis;
+    }
+
+    public void setMinEvictableIdleTimeMillis(int minEvictableIdleTimeMillis) {
+        this.minEvictableIdleTimeMillis = minEvictableIdleTimeMillis;
+    }
+
+    public void setValidationQuery(String validationQuery) {
+        this.validationQuery = validationQuery;
+    }
+
+    public void setTestWhileIdle(boolean testWhileIdle) {
+        this.testWhileIdle = testWhileIdle;
+    }
+
+    public void setTestOnBorrow(boolean testOnBorrow) {
+        this.testOnBorrow = testOnBorrow;
+    }
+
+    public void setTestOnReturn(boolean testOnReturn) {
+        this.testOnReturn = testOnReturn;
+    }
+
+    public void setPoolPreparedStatements(boolean poolPreparedStatements) {
+        this.poolPreparedStatements = poolPreparedStatements;
+    }
+
+    public void setMaxPoolPreparedStatementPerConnectionSize(int maxPoolPreparedStatementPerConnectionSize) {
+        this.maxPoolPreparedStatementPerConnectionSize = maxPoolPreparedStatementPerConnectionSize;
+    }
+
+    public void setFilters(String filters) {
+        this.filters = filters;
+    }
+
+    public void setLoginUsername(String loginUsername) {
+        this.loginUsername = loginUsername;
+    }
+
+    public void setLoginPassword(String loginPassword) {
+        this.loginPassword = loginPassword;
+    }
+
+    public void setResetEnable(String resetEnable) {
+        this.resetEnable = resetEnable;
+    }
+
+    public void setAllow(String allow) {
+        this.allow = allow;
+    }
+
+    public void setDeny(String deny) {
+        this.deny = deny;
     }
 }
